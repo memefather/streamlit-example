2356d27427151fdfca799bed7153a821e8fb4b3b
memefather <142188887+memefather@users.noreply.github.com>
2023-08-13 20:08:54 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,36 +3,47 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import openai
 
 """
-# Welcome to Streamlit!🚀
+# Welcome to SongQuiz! 🚀
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+What's the lyrics again? Let's test your musical knowledge!
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
 """
+openai.api_key = os.getenv("OPENAI_API_KEY")
+model_id = 'gpt-3.5-turbo'
+
+def ChatGPT_conversation(conversation):
+    response = openai.ChatCompletion.create(
+        model=model_id,
+        messages=conversation
+    )
+    # api_usage = response['usage']
+    # print('Total token consumed: {0}'.format(api_usage['total_tokens']))
+    # stop means complete
+    # print(response['choices'][0].finish_reason)
+    # print(response['choices'][0].index)
+    conversation.append({'role': response.choices[0].message.role, 'content': response.choices[0].message.content})
+    return conversation
+
+conversation = []
+conversation.append({'role': 'system', 'content': 'You are playing a game with the user. The user will give you a line of lyric from a song. You will return the next line.'})
+conversation = ChatGPT_conversation(conversation)
+print('{0}: {1}\n'.format(conversation[-1]['role'].strip(), conversation[-1]['content'].strip()))
+
+def singnow():
+    input = st.text_input('', 'Near, far, wheverever you are')
+
+while input != '':
+    prompt = input
+    conversation.append({'role': 'user', 'content': prompt})
+    conversation = ChatGPT_conversation(conversation)
+    print('{0}: {1}\n'.format(conversation[-1]['role'].strip(), conversation[-1]['content'].strip()))
+
+if __name__ == '__main__':
+    # call main function
+    singnow()
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    
